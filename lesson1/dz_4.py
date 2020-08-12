2cf7d10df411d0d6b9df73df9379137c477b65cf
AlexLisecky <alexlisecky@gmail.com>
2020-08-12 22:43:02 +0300

    delete

--- lesson1/dz_4.py
+++
@@ -1,8 +0,0 @@
-number = int(input("Введите целое положительное число: "))
-max = 0
-while number > 0:
-    last = number % 10
-    number = number // 10
-    if last > max:
-        max = last
-print(max)
